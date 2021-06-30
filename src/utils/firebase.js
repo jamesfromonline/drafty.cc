6063e97b88a715a0dc9141fb1bca7e3607d5e0a1
jamesfromonline <jtwebco@gmail.com>
2021-06-30 15:24:42 -0400

    new env, updates

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyBzsPE1p7qXMw-TDkn_tH5TSek21ISSxds",
-  authDomain: "twotter-e6df5.firebaseapp.com",
-  databaseURL: "https://twotter-e6df5.firebaseio.com",
-  projectId: "twotter-e6df5",
-  storageBucket: "twotter-e6df5.appspot.com",
-  messagingSenderId: "7670825799",
-  appId: "1:7670825799:web:9a61246e75c87c25a58262"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "speek2-5f7d5.firebaseapp.com",
+  databaseURL: "https://speek2-5f7d5.firebaseio.com",
+  projectId: "speek2-5f7d5",
+  storageBucket: "speek2-5f7d5.appspot.com",
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APPID
 }
 
 firebase.initializeApp(config)
